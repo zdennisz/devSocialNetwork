ef83226c2c2a7ff26f284bd38d475ce63d41ea10
Dennis Z <zdennisz@gmail.com>
2021-10-27 13:42:05 +0300

    connected mongoDb with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('API running'))
+const express = require('express');
+const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+const app = express();
+//Connect Database
+connectDB();
+
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
