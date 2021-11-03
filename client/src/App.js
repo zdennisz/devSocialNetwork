7ae6893b8d45db358dc3162898ac3b33f18226f8
Dennis Z <zdennisz@gmail.com>
2021-11-03 12:43:17 +0200

    added loading of user& setting auth token of user

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,40 @@
 import "./App.css";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
-
+import { loadUser } from "./store/actions/auth";
 import { Provider } from "react-redux";
 import store from "./store/store";
-const App = () => (
-	<Provider store={store}>
-		<Router>
-			<Navbar />
-			<Route exact path='/' component={Landing} />
-			<section className='container'>
-				<Alert />
-				<Switch>
-					<Route exact path='/register' component={Register} />
-					<Route exact path='/login' component={Login} />
-				</Switch>
-			</section>
-		</Router>
-	</Provider>
-);
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
+const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
+	return (
+		<Provider store={store}>
+			<Router>
+				<Navbar />
+				<Route exact path='/' component={Landing} />
+				<section className='container'>
+					<Alert />
+					<Switch>
+						<Route exact path='/register' component={Register} />
+						<Route exact path='/login' component={Login} />
+					</Switch>
+				</section>
+			</Router>
+		</Provider>
+	);
+};
 
 export default App;
--- client/src/store/reducers/auth.js
+++ client/src/store/reducers/auth.js
@@ -1,4 +1,9 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import {
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+} from "../actions/types";
 
 const initialState = {
 	token: localStorage.getItem("token"),
@@ -19,6 +24,7 @@ const authReducer = (state = initialState, action) => {
 				loading: false,
 			};
 		case REGISTER_FAIL:
+		case AUTH_ERROR:
 			localStorage.removeItem("token");
 			return {
 				...state,
@@ -26,6 +32,14 @@ const authReducer = (state = initialState, action) => {
 				isAuthenticated: false,
 				loading: false,
 			};
+		case USER_LOADED:
+			return {
+				...state,
+				isAuthenticated: true,
+				loading: false,
+				user: payload,
+			};
+
 		default:
 			return state;
 	}
