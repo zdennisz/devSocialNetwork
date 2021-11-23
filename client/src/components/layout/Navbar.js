dda77706dd323736f7a0f424887b2255b11ac641
Dennis Z <zdennisz@gmail.com>
2021-11-23 13:38:25 +0200

    added redirect in case user is authenticated

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,6 +14,12 @@ const Navbar = () => {
 		return (
 			<ul>
 				<li>
+					<Link to='/dashboard'>
+						<i className='fas fa-user' />
+						<span className='hide-sm'> Dashboard</span>
+					</Link>
+				</li>
+				<li>
 					<a onClick={logoutHandler} href='#!'>
 						<i className='fas fa-sign-out-alt' />
 						<span className='hide-sm'> Logout</span>
