7a2e9df6c7348a9aa58d9f8f4c06a5e79edfed7b
Dennis Z <zdennisz@gmail.com>
2021-11-25 10:21:22 +0200

    added the singular post item

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
+import { useSelector, useDispatch } from "react-redux";
+
+const PostItem = ({
+	post: { _id, text, name, avatar, user, likes, comments, date },
+}) => {
+	const auth = useSelector((state) => state.auth);
+	return (
+		<div className='post bg-white p-1 my-1'>
+			<div>
+				<a href='profile.html'>
+					<img className='round-img' src={avatar} alt='' />
+					<h4>{name}</h4>
+				</a>
+			</div>
+			<div>
+				<p className='my-1'>{text}</p>
+				<p className='post-date'>
+					Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
+				</p>
+				<button type='button' className='btn btn-light'>
+					<i className='fas fa-thumbs-up' />{" "}
+					<span>{likes.lenth > 0 && <span>{likes.legnth}</span>}</span>
+				</button>
+				<button type='button' className='btn btn-light'>
+					<i className='fas fa-thumbs-down'></i>
+				</button>
+				<Link to={`/post/${_id}`} className='btn btn-primary'>
+					Discussion{" "}
+					{comments.lenth > 0 && (
+						<span className='comment-count'>{comments.legnth}</span>
+					)}
+				</Link>
+				{!auth.loading && user === auth.user._id && (
+					<button type='button' className='btn btn-danger'>
+						<i className='fas fa-times'></i>
+					</button>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default PostItem;
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { getPosts } from "../../store/actions/post";
 import { useDispatch, useSelector } from "react-redux";
+import PostItem from "./PostItem";
 import Spinner from "../layout/Spinner";
 const Posts = () => {
 	const dispatch = useDispatch();
@@ -9,7 +10,21 @@ const Posts = () => {
 		dispatch(getPosts());
 	}, [dispatch]);
 
-	return <div></div>;
+	return loading ? (
+		<Spinner />
+	) : (
+		<>
+			<h1 className='large text-primary'>Posts</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i>Welcome to the community
+			</p>
+			<div className='posts'>
+				{posts.map((post) => (
+					<PostItem key={post._id} post={post} />
+				))}
+			</div>
+		</>
+	);
 };
 
 export default Posts;
